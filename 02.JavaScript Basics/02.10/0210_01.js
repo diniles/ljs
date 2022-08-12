8d130850b4c492df0a06d4295db0d7ed39381a83
diniles <dzliashko@gmail.com>
2022-08-12 14:54:14 +0300

    Add: Chapter 02.10 "Условное ветвление: if, '?'"

---
+++ 02.JavaScript Basics/02.10/0210_01.js
@@ -0,0 +1,7 @@
+'use strict';
+const jsName = prompt('Какое «официальное» название JavaScript?', 'ECMAScript');
+if (jsName == 'ECMAScript') {
+  alert('Верно!');
+} else {
+  alert('Не знаете? ECMAScript!');
+}
---
+++ 02.JavaScript Basics/02.10/0210_02.js
@@ -0,0 +1,9 @@
+'use strict';
+const num = +prompt('Enter a number');
+if (num > 0) {
+  alert(1);
+} else if (num < 0) {
+  alert(-1);
+} else {
+  alert(0);
+}
