ebaf89b154fdcf1f663e73d653cc86c041dbc84e
Dzmitry Liashko <dzliashko@gmail.com>
2022-08-12 22:05:04 +0300

    Add: Chapter 02.13 "Циклы while и for"

---
+++ 02.JavaScript Basics/02.13/0213_01.js
@@ -0,0 +1,15 @@
+'use strict';
+// Выведите чётные числа
+// При помощи цикла for выведите чётные числа от 2 до 10.
+
+for (let i = 2; i <= 10; i += 2) {
+  console.log(i);
+}
+
+// solution from site
+//why so complicated?
+for (let i = 2; i <= 10; i++) {
+  if (i % j == 0) {
+    console.log(i);
+  }
+}
