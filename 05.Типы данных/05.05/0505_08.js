a834c0090766d9005b2b187d9aab4d43eb05810b
Dzmitry Liashko <dzliashko@gmail.com>
2022-08-21 13:17:49 +0300

    Add: Chapter 05.05 'Методы массивов' finished

--- 05.Типы данных/05.05/0505_08.js
+++ 05.Типы данных/05.05/0505_08.js
@@ -27,3 +27,17 @@
 // alert( usersMapped[0].fullName ) // Вася Пупкин
 // Итак, на самом деле вам нужно трансформировать один массив объектов в другой. Попробуйте использовать =>.
 // Это небольшая уловка.
+
+let vasya = {name: "Вася", surname: "Пупкин", id: 1};
+let petya = {name: "Петя", surname: "Иванов", id: 2};
+let masha = {name: "Маша", surname: "Петрова", id: 3};
+
+let users = [vasya, petya, masha];
+
+let usersMapped = users.map(user => ({
+  fullName: `${user.name} ${user.surname}`,
+  id: user.id
+}));
+
+console.log(usersMapped[0].id);
+console.log(usersMapped[0].fullName);
--- 05.Типы данных/05.05/0505_09.js
+++ 05.Типы данных/05.05/0505_09.js
@@ -12,7 +12,23 @@
 //
 // sortByAge(arr);
 //
-// // теперь: [vasya, masha, petya]
+// теперь: [vasya, masha, petya]
 // alert(arr[0].name); // Вася
 // alert(arr[1].name); // Маша
 // alert(arr[2].name); // Петя
+
+let vasya = {name: "Вася", age: 25};
+let petya = {name: "Петя", age: 30};
+let masha = {name: "Маша", age: 28};
+
+let arr = [vasya, petya, masha];
+
+function sortByAge(users) {
+  users.sort((a, b) => a.age - b.age);
+}
+
+sortByAge(arr);
+
+console.log(arr[0].name); // Вася
+console.log(arr[1].name); // Маша
+console.log(arr[2].name); // Петя
