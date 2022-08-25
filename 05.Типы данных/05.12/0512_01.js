bc6f37c8a0ea6214ace22a4e889dd9f8bab1c1be
diniles <dzliashko@gmail.com>
2022-08-25 11:40:02 +0300

    Add: 0512_01.js solved

--- 05.Типы данных/05.12/0512_01.js
+++ 05.Типы данных/05.12/0512_01.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 // Преобразуйте объект в JSON, а затем обратно в обычный объект
 // Преобразуйте user в JSON, затем прочитайте этот JSON в другую переменную.
 
@@ -6,3 +6,14 @@
 //   name: "Василий Иванович",
 //   age: 35
 // };
+
+let user = {
+  name: "Василий Иванович", age: 35
+};
+console.log(user);
+
+const userJson = JSON.stringify(user);
+console.log(userJson);
+
+const userFromJson = JSON.parse(userJson);
+console.log(userFromJson);
