29d29ddb226513ff81c515ba9c25c0cf27787b24
diniles <dzliashko@gmail.com>
2022-08-26 13:20:44 +0300

    Fix: 0511_06.js solved

--- 05.Типы данных/05.11/0511_06.js
+++ 05.Типы данных/05.11/0511_06.js
@@ -6,3 +6,32 @@
 //
 // getSecondsToday() == 36000 // (3600 * 10)
 // Функция должна работать в любой день, т.е. в ней не должно быть конкретного значения сегодняшней даты.
+
+// My solution
+function getSecondsToday() {
+  let currTime = Date.now();
+  let currYear = new Date().getFullYear();
+  console.log(currYear);
+  let currMonth = new Date().getMonth();
+  console.log(currMonth);
+  let currDate = new Date().getDate();
+  console.log(currDate);
+  let zeroTime = new Date(currYear, currMonth, currDate, 0, 0, 0, 0);
+  console.log(zeroTime);
+  return (currTime - zeroTime) / 1000;
+
+}
+
+// Best solution
+function getSecondsTodayGood() {
+  let now = new Date();
+
+  // создаём объект с текущими днём/месяцем/годом
+  let today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+
+  let diff = now - today; // разница в миллисекундах
+  return Math.round(diff / 1000); // получаем секунды
+}
+
+console.log(getSecondsToday());
+console.log(getSecondsTodayGood());
