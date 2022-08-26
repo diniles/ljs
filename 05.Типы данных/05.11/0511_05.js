0a9fb4e59c23e546677fb31edc459771484102dc
diniles <dzliashko@gmail.com>
2022-08-26 08:57:18 +0300

    Fix: 0511_05.js solved

--- 05.Типы данных/05.11/0511_05.js
+++ 05.Типы данных/05.11/0511_05.js
@@ -8,3 +8,29 @@
 // year – год из четырёх цифр, например, 2012.
 // month – месяц от 0 до 11.
 // К примеру, getLastDayOfMonth(2012, 1) = 29 (високосный год, февраль).
+
+function getLastDayOfMonth(year, month) {
+  switch (month) {
+    case 1:
+      return leap(year) ? 29 : 28;
+    case 0:
+    case 2:
+    case 4:
+    case 6:
+    case 7:
+    case 9:
+    case 11:
+      return 31;
+    default:
+      return 30;
+  }
+
+  function leap(year) {
+    return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+  }
+}
+
+console.log(getLastDayOfMonth(2012, 1));
+console.log(getLastDayOfMonth(2011, 1));
+console.log(getLastDayOfMonth(2012, 0));
+console.log(getLastDayOfMonth(2012, 3));
