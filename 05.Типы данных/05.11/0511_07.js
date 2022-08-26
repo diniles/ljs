55541a4bf256656bdbab5ff46cbd774ff7117bd2
diniles <dzliashko@gmail.com>
2022-08-26 13:26:07 +0300

    Fix: 0511_07.js solved

--- 05.Типы данных/05.11/0511_07.js
+++ 05.Типы данных/05.11/0511_07.js
@@ -6,3 +6,25 @@
 //
 // getSecondsToTomorrow() == 3600
 // P.S. Функция должна работать в любой день, т.е. в ней не должно быть конкретного значения сегодняшней даты.
+
+
+// My solution
+function getSecondsToTomorrow() {
+  let now = new Date();
+  let todayNight = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 23, 59, 59);
+  return Math.round((todayNight - now) / 1000);
+}
+
+// Correct solution
+function getSecondsToTomorrowBest() {
+  let now = new Date();
+
+  // завтрашняя дата
+  let tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
+
+  let diff = tomorrow - now; // разница в миллисекундах
+  return Math.round(diff / 1000); // преобразуем в секунды
+}
+
+console.log(getSecondsToTomorrow());
+console.log(getSecondsToTomorrowBest());
