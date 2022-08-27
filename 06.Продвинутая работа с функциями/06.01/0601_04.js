ef3be2e31b4b7909ba3d8bc3fdcc311e47f9054d
Dzmitry Liashko <dzliashko@gmail.com>
2022-08-27 20:02:16 +0300

    Add: 0601_04.js solved

--- 06.Продвинутая работа с функциями/06.01/0601_04.js
+++ 06.Продвинутая работа с функциями/06.01/0601_04.js
@@ -20,3 +20,36 @@
 // Сделайте два варианта решения: используя цикл и через рекурсию.
 //
 // Как лучше: с рекурсией или без?
+
+let list = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: 4,
+        next: null
+      }
+    }
+  }
+};
+
+function printListCircle(list) {
+  let tmp = list;
+
+  while (tmp) {
+    console.log(tmp.value);
+    tmp = tmp.next;
+  }
+}
+
+function printListRecursion(list) {
+  while (list) {
+    console.log(list.value);
+    list = list.next;
+  }
+}
+
+printListCircle(list);
+printListRecursion(list);
