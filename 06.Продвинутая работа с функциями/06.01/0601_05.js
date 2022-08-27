516bc92a6ee9c28a78b552e69279b1b205b3e1d1
Dzmitry Liashko <dzliashko@gmail.com>
2022-08-27 20:07:30 +0300

    Add: 0601_05.js solution from site not working

--- 06.Продвинутая работа с функциями/06.01/0601_05.js
+++ 06.Продвинутая работа с функциями/06.01/0601_05.js
@@ -2,4 +2,57 @@
 // Вывод односвязного списка в обратном порядке
 // Выведите односвязный список из предыдущего задания Вывод односвязного списка в обратном порядке.
 //
+// let list = {
+//   value: 1,
+//   next: {
+//     value: 2,
+//     next: {
+//       value: 3,
+//       next: {
+//         value: 4,
+//         next: null
+//       }
+//     }
+//   }
+// };
 // Сделайте два решения: с использованием цикла и через рекурсию.
+
+let list = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: 4,
+        next: null
+      }
+    }
+  }
+};
+
+function printReverseListRecursion(list) {
+
+  if (list.next) {
+    printReverseList(list.next);
+  }
+
+  console.log(list.value);
+}
+
+function printReverseListCycle(list) {
+  let arr = [];
+  let tmp = list;
+
+  while (tmp) {
+    arr.push(tmp.value);
+    tmp = tmp.next;
+  }
+
+  for (let i = arr.length - 1; i >= 0; i--) {
+    console.log(arr[i]);
+  }
+}
+
+printReverseListCycle();
+printReverseListRecursion();
