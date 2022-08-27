8e7f0c35bdec777eb67be3c7b887d1e28b3fb320
Dzmitry Liashko <dzliashko@gmail.com>
2022-08-27 19:14:18 +0300

    Add: 0511_08.js solved

--- 05.Типы данных/05.11/0511_08.js
+++ 05.Типы данных/05.11/0511_08.js
@@ -16,4 +16,26 @@
 // alert( formatDate(new Date(new Date - 5 * 60 * 1000)) ); // "5 мин. назад"
 //
 // // вчерашняя дата вроде 31.12.2016, 20:00
-// alert( formatDate(new Date(new Date - 86400 * 1000)) );\
+// alert( formatDate(new Date(new Date - 86400 * 1000)) );
+
+function formatDate(date) {
+  let diff = Date.now() - date;
+  if (diff < 1000) {
+    return 'прямо сейчас';
+  } else if (diff < 1000 * 60) {
+    return `${diff / 1000} сек. назад`;
+  } else if (diff < 1000 * 60 * 60) {
+    return `${diff / (1000 * 60)} мин. назад`;
+  } else {
+    return diff;
+  }
+
+}
+
+console.log((formatDate(new Date(new Date - 1)))); // "прямо сейчас"
+
+console.log((formatDate(new Date(new Date - 30 * 1000)))); // "30 сек. назад"
+
+console.log((formatDate(new Date(new Date - 5 * 60 * 1000)))); // "5 мин. назад"
+
+console.log((formatDate(new Date(new Date - 86400 * 1000))));
