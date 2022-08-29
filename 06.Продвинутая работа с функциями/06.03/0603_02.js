fb42bab52ce3ad24db2b376c710bd0c8ab7d589b
diniles <dzliashko@gmail.com>
2022-08-29 14:12:15 +0300

    Add: Chapter 06.04 "Устаревшее ключевое слово "var""

--- 06.Продвинутая работа с функциями/06.03/0603_02.js
+++ 06.Продвинутая работа с функциями/06.03/0603_02.js
@@ -19,3 +19,13 @@
 // alert( arr.filter(inBetween(3, 6)) ); // 3,4,5,6
 //
 // alert( arr.filter(inArray([1, 2, 10])) ); // 1,2
+
+let arr = [1, 2, 3, 4, 5, 6, 7];
+
+function inBetween(x, a, b) {
+  return x >= a && x <= b;
+}
+
+console.log(arr.filter(inBetween(3, 6))); // 3,4,5,6
+
+// alert(arr.filter(inArray([1, 2, 10]))); // 1,2
