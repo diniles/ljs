680a72d0b43356e55dae2515788239b62a7ffe89
diniles <dzliashko@gmail.com>
2022-08-29 14:16:58 +0300

    Add: task 0603_02.js solved

--- 06.Продвинутая работа с функциями/06.03/0603_02.js
+++ 06.Продвинутая работа с функциями/06.03/0603_02.js
@@ -22,10 +22,18 @@
 
 let arr = [1, 2, 3, 4, 5, 6, 7];
 
-function inBetween(x, a, b) {
-  return x >= a && x <= b;
+function inBetween(a, b) {
+  return function (x) {
+    return x >= a && x <= b;
+  };
+}
+
+function inArray(arr) {
+  return function (x) {
+    return arr.includes(x);
+  };
 }
 
 console.log(arr.filter(inBetween(3, 6))); // 3,4,5,6
 
-// alert(arr.filter(inArray([1, 2, 10]))); // 1,2
+console.log(arr.filter(inArray([1, 2, 10]))); // 1,2
