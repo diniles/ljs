991fe0eb4ad33547b1bbf2c1d1d7b22af3a4b9d4
diniles <dzliashko@gmail.com>
2022-08-29 14:20:46 +0300

    Add: task 0603_03.js solved

--- 06.Продвинутая работа с функциями/06.03/0603_03.js
+++ 06.Продвинутая работа с функциями/06.03/0603_03.js
@@ -21,3 +21,16 @@
 // То есть чтобы вместо функции мы просто писали byField(fieldName).
 //
 //   Напишите функцию byField, которая может быть использована для этого.
+
+let users = [
+  {name: "John", age: 20, surname: "Johnson"},
+  {name: "Pete", age: 18, surname: "Peterson"},
+  {name: "Ann", age: 19, surname: "Hathaway"}
+];
+
+function byField(field) {
+  users.sort((a, b) => a[field] > b[field] ? 1 : -1);
+}
+
+console.log(users.sort(byField('name')));
+console.log(users.sort(byField('age')));
