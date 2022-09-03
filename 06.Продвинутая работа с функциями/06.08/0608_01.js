53cda8a7c6f0a7675d5622c9b6f9343a55119075
diniles <dzliashko@gmail.com>
2022-09-03 14:06:43 +0300

    Add: 0608_01.js solved

--- 06.Продвинутая работа с функциями/06.08/0608_01.js
+++ 06.Продвинутая работа с функциями/06.08/0608_01.js
@@ -6,3 +6,33 @@
 //
 // Используя setInterval.
 // Используя рекурсивный setTimeout.
+
+function printNumbers(from, to) {
+  let current = from;
+
+  let timerId = setInterval(function () {
+    console.log(current);
+    if (current == to) {
+      clearInterval(timerId);
+    }
+    current++;
+  }, 1000);
+}
+
+// использование:
+printNumbers(5, 10);
+
+function printNumbers(from, to) {
+  let current = from;
+
+  setTimeout(function go() {
+    console.log(current);
+    if (current < to) {
+      setTimeout(go, 1000);
+    }
+    current++;
+  }, 1000);
+}
+
+// использование:
+printNumbers(5, 10);
