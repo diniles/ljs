667b39ca11239d9791b9e75d22b82f66c5aaf744
diniles <dzliashko@gmail.com>
2022-09-07 15:51:06 +0300

    Add: 0609_03.js solved

--- 06.Продвинутая работа с функциями/06.09/0609_03.js
+++ 06.Продвинутая работа с функциями/06.09/0609_03.js
@@ -16,3 +16,24 @@
 // setTimeout( () => f(5), 1500); // проигнорирован (прошло только 400 мс от последнего вызова)
 // На практике debounce полезен для функций, которые получают/обновляют данные, и мы знаем, что повторный вызов в
 // течение короткого промежутка времени не даст ничего нового. Так что лучше не тратить на него ресурсы.
+
+function debounce(f, ms) {
+
+  let isCooldown = false;
+
+  return function () {
+    if (isCooldown) return;
+
+    f.apply(this, arguments);
+
+    isCooldown = true;
+
+    setTimeout(() => isCooldown = false, ms);
+  };
+
+}
+
+let f = debounce(console.log, 1000);
+
+f(1); // выполняется немедленно
+f(2); // проигнорирован
