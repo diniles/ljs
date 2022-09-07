71f010d165510f08b874737f77b85b0bfcce03fd
diniles <dzliashko@gmail.com>
2022-09-07 15:52:29 +0300

    Add: 0609_04.js solved

--- 06.Продвинутая работа с функциями/06.09/0609_04.js
+++ 06.Продвинутая работа с функциями/06.09/0609_04.js
@@ -47,3 +47,44 @@
 // // когда 1000 мс истекли ...
 // // ...выводим 3, промежуточное значение 2 было проигнорировано
 // P.S. Аргументы и контекст this, переданные в f1000, должны быть переданы в оригинальную f.
+
+function throttle(func, ms) {
+
+  let isThrottled = false,
+    savedArgs,
+    savedThis;
+
+  function wrapper() {
+
+    if (isThrottled) { // (2)
+      savedArgs = arguments;
+      savedThis = this;
+      return;
+    }
+
+    func.apply(this, arguments); // (1)
+
+    isThrottled = true;
+
+    setTimeout(function () {
+      isThrottled = false; // (3)
+      if (savedArgs) {
+        wrapper.apply(savedThis, savedArgs);
+        savedArgs = savedThis = null;
+      }
+    }, ms);
+  }
+
+  return wrapper;
+}
+
+function f(a) {
+  console.log(a);
+}
+
+// f1000 передаёт вызовы f максимум раз в 1000 мс
+let f1000 = throttle(f, 1000);
+
+f1000(1); // показывает 1
+f1000(2); // (ограничение, 1000 мс ещё нет)
+f1000(3); // (ограничение, 1000 мс ещё нет)
