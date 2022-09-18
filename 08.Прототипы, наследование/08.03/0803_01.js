295d0352915f40c1d567e62f5a3dace8d0e4d937
Dzmitry Liashko <dzliashko@gmail.com>
2022-09-18 16:14:12 +0300

    Add: 0803_01.js task solved

--- 08.Прототипы, наследование/08.03/0803_01.js
+++ 08.Прототипы, наследование/08.03/0803_01.js
@@ -9,3 +9,13 @@
 //   }
 //
 // f.defer(1000); // выведет "Hello!" через 1 секунду
+
+Function.prototype.defer = function (ms) {
+  setTimeout(this, ms);
+};
+
+function f() {
+  console.log("Hello!");
+}
+
+f.defer(1000);
