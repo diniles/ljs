2709a2d03e84f1800599468287672b9a5dcedee2
Dzmitry Liashko <dzliashko@gmail.com>
2022-09-18 16:18:37 +0300

    Add: 0804_01.js task solved

--- 08.Прототипы, наследование/08.04/0804_01.js
+++ 08.Прототипы, наследование/08.04/0804_01.js
@@ -22,3 +22,22 @@
 //
 // // ваш метод toString в действии
 // alert(dictionary); // "apple,__proto__"
+
+let dictionary = Object.create(null, {
+  toString: { // определяем свойство toString
+    value() { // значение -- это функция
+      return Object.keys(this).join();
+    }
+  }
+});
+
+dictionary.apple = "Apple";
+dictionary.__proto__ = "test";
+
+// apple и __proto__ выведены в цикле
+for (let key in dictionary) {
+  console.log(key); // "apple", затем "__proto__"
+}
+
+// список свойств, разделённых запятой, выведен с помощью toString
+console.log(dictionary); // "apple,__proto__"
