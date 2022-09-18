46c6d5cbad9f675a87654682d7baad9125b47c5e
Dzmitry Liashko <dzliashko@gmail.com>
2022-09-18 16:15:16 +0300

    Add: 0803_02.js task solved

--- 08.Прототипы, наследование/08.03/0803_02.js
+++ 08.Прототипы, наследование/08.03/0803_02.js
@@ -11,3 +11,16 @@
 //
 // f.defer(1000)(1, 2); // выведет 3 через 1 секунду.
 // Пожалуйста, заметьте, что аргументы должны корректно передаваться оригинальной функции.
+Function.prototype.defer = function (ms) {
+  let f = this;
+  return function (...args) {
+    setTimeout(() => f.apply(this, args), ms);
+  };
+};
+
+// check it
+function f(a, b) {
+  console.log(a + b);
+}
+
+f.defer(1000)(1, 2);
