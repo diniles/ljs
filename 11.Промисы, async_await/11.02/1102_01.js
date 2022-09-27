49c7813ab49e264ff7d48be4e2673c1ff3a7ef4c
diniles <dzliashko@gmail.com>
2022-09-27 11:08:17 +0300

    Add: task 1102_01 solved

--- 11.Промисы, async_await/11.02/1102_01.js
+++ 11.Промисы, async_await/11.02/1102_01.js
@@ -14,6 +14,9 @@
 
 function delay(ms) {
   // ваш код
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve(), ms);
+  });
 }
 
 delay(3000).then(() => console.log('выполнилось через 3 секунды'));
