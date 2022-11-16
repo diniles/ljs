cdcf95c680f08d36c460fe0c7d843b6afe0049c3
diniles <dzliashko@gmail.com>
2022-11-16 15:44:44 +0300

    Add: 0107 "Изменение документа"

---
+++ 02.Браузер документ, события, интерфейсы/01.Документ/01.07/0107_06.js
@@ -0,0 +1,10 @@
+'use strict';
+// Цветные часы с использованием setInterval
+// важность: 4
+// Создайте цветные часы как в примере ниже:
+
+// hh(red color):mm(green colr):ss(blue color)
+// [button "Start"][button "Stop"]
+
+// Для стилизации используйте HTML/CSS, JavaScript должен только обновлять время
+// в элементах.
---
+++ 02.Браузер документ, события, интерфейсы/01.Документ/01.07/0107_08.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// Сортировка таблицы
+// важность: 5
+// Вот таблица:
+
+// <table>
+// <thead>
+//   <tr>
+//     <th>Name</th><th>Surname</th><th>Age</th>
+//   </tr>
+// </thead>
+// <tbody>
+//   <tr>
+//     <td>John</td><td>Smith</td><td>10</td>
+//   </tr>
+//   <tr>
+//     <td>Pete</td><td>Brown</td><td>15</td>
+//   </tr>
+//   <tr>
+//     <td>Ann</td><td>Lee</td><td>5</td>
+//   </tr>
+//   <tr>
+//     <td>...</td><td>...</td><td>...</td>
+//   </tr>
+// </tbody>
+// </table>
+// В ней может быть больше строк.
+
+// Напишите код для сортировки по столбцу "name".
